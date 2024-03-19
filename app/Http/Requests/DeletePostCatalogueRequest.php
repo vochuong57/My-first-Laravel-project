241dfdc34c73b7167298c77a7a890f37f4bc41b5
vochuong57 <vochuong57@gmail.com>
2024-03-19 18:24:54 +0700

    v14

---
+++ app/Http/Requests/DeletePostCatalogueRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Models\PostCatalogue;
+use App\Rules\CheckPostCatalogueChildrenRule;
+
+class DeletePostCatalogueRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $postCatalogueId= $this->route('id');
+        //echo $postCatalogueId; die();
+        return [
+            'name'=>[
+                new CheckPostCatalogueChildrenRule($postCatalogueId)
+            ],
+        ];
+    }
+}
