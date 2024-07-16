1fda1922ec28a7355da440907c803f371fa78ef9
vochuong57 <vochuong57@gmail.com>
2024-07-16 16:46:45 +0700

    V45-Fix-bugs-kiểm-tra-bản-dịch-danh-mục-P2

---
+++ app/Templates/Model/TemplateModelCatalogueModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class {ModuleTemplate}Catalogue{ModuleTemplate} extends Model
+{
+    use HasFactory;
+
+    protected $table='{moduleTemplate}_catalogue_{moduleTemplate}';
+}
