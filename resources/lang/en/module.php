6797aa277515a4d9008d2d625b791713c4b2488b
vochuong57 <vochuong57@gmail.com>
2024-09-19 17:49:48 +0700

    V97-Xây-dựng-chức-năng-quản-lý-khuyến-mãi---Phần-4

--- resources/lang/en/module.php
+++ resources/lang/en/module.php
@@ -10,5 +10,28 @@ return [
     'type'=>[
         'dropdown-menu' => 'Dropdown Menu',
         'mega-menu' => 'Mega Menu'
+    ],
+    // V74
+    'effect' => [
+        'fade' => 'Fade',
+        'cube' => 'Cube',
+        'coverflow' => 'Coverflow',
+        'flip' => 'Flip',
+        'cards' => 'Cards',
+        'creative' => 'Creative'
+    ],
+    'navigate' => [
+        'hide' => 'Hide navigation bar',
+        'dots' => 'Show as dots',
+        'thumbnails' => 'Show as Thumbnails'
+    ],
+
+    // V97
+    'promotion' => [
+        'order_amount_range' => 'Discount by total order value',
+        'product_and_quantity' => 'Discount by individual product',
+        'product_quantity_range' => 'Discount by product quantity',
+        'goods_discount_by_quantity' => 'Buy product - discount product'
     ]
+
 ];
